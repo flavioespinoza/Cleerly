b145ac77c1471abd00904147937c7a4f3e54b09c
flavioespinoza <flavio.espinoza@gmail.com>
2017-07-28 14:00:44 -0700

    Cleaned up directories and added notes to flavor.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cleerly
 Dev test for Cleerly Medical
+
+## How to use
+
+Clone or Download files. Open index.html in Chrome. Have fun.
--- flavor.js
+++ flavor.js
@@ -2,8 +2,7 @@
  * Created by Flavor on 7/28/17.
  */
 
-var worker = new Worker('worker.js');
-
+/** Main image function that calls the mosaic filter. */
 function processImage (_tile_width, _tile_height, _image_id, _target_id) {
   'use strict'
   var image = document.getElementById(_image_id)
@@ -17,12 +16,21 @@ function processImage (_tile_width, _tile_height, _image_id, _target_id) {
   })
 }
 
+/** Create web worker to process images on multiple threads. */
+var worker = new Worker('worker.js');
+worker.onmessage = function (event) {
+  var msg = JSON.parse(event.data);
+  processImage(msg.tileHeight, msg.tileWidth, msg.imageId, msg.targetId);
+};
+
+/** Load first image and render. */
 $(document).ready(function () {
   processImage(4, 4, 'goni', 'target')
   $('.title').html('goni')
   $('.range-title').html(4)
 })
 
+/** Select avatar and render. */
 $('.img-avatar').on('click', function (e) {
   $('#target').html('')
   $('.title').html(this.id)
@@ -31,15 +39,12 @@ $('.img-avatar').on('click', function (e) {
   }));
 })
 
-worker.onmessage = function (event) {
-  var msg = JSON.parse(event.data);
-  processImage(msg.tileHeight, msg.tileWidth, msg.imageId, msg.targetId);
-};
-
+/** Tile size input restricted to numeric inputs only. */
 $('#pixleInput').on('keydown', '#pixleSize', function (e) {
   -1 !== $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) || /65|67|86|88/.test(e.keyCode) && (!0 === e.ctrlKey || !0 === e.metaKey) || 35 <= e.keyCode && 40 >= e.keyCode || (e.shiftKey || 48 > e.keyCode || 57 < e.keyCode) && (96 > e.keyCode || 105 < e.keyCode) && e.preventDefault()
 });
 
+/** Update function to change the tile sizes. */
 $('.update').on('click', function () {
   var n = Number($('#pixleSize').val());
   var currentId = $('.title').text();
